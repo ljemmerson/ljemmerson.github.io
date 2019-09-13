69d541ba901aa7216eb6819220460f6feccb94fa
Luke Emmerson <ljemmerson@gmail.com>
2019-09-13 18:26:07 +0100

    updating nav

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,22 @@
 (function() {
-  console.log("test");
+  $('#casestudy-link').click(function() {
+    $('html,body').animate({
+      scrollTop: $('.footer_casestudies').offset().top
+    }, 'slow');
+  });
+
+  $(function() {
+    var header;
+    header = $('.nav-container');
+    $(window).scroll(function() {
+      var scroll;
+      scroll = $(window).scrollTop();
+      if (scroll >= 44) {
+        $('.nav-container').addClass('nav-scrolling');
+      } else {
+        $('.nav-container').removeClass('nav-scrolling');
+      }
+    });
+  });
 
 }).call(this);
